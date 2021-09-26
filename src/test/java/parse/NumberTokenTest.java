83df50d5734ceac406ec6c2df9a8b394aafc95e7
Tim Egenton <tje49@drexel.edu>
2021-09-26 13:44:21 -0400

    Number digits

---
+++ src/test/java/parse/NumberTokenTest.java
@@ -0,0 +1,26 @@
+package parse;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class NumberTokenTest {
+    public static NumberToken numberToken;
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+            "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"})
+    void validNumbers(String s) {
+        numberToken = (NumberToken) NumberToken.fromString(s).orElse(null);
+        assertNotNull(numberToken);
+        assertEquals(s, numberToken.getWord().getWord());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Whenever", "five", "X"})
+    void invalid(String s) {
+        numberToken = (NumberToken) NumberToken.fromString(s).orElse(null);
+        assertNull(numberToken);
+    }
+}
