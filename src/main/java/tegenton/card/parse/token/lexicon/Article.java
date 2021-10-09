8bd7b17e8bc6730a66182c3e5b707fa218a208e0
Tim Egenton <22758764+tegenton@users.noreply.github.com>
2021-10-09 14:25:51 -0400

    Article linting

--- src/main/java/tegenton/card/parse/token/lexicon/Article.java
+++ src/main/java/tegenton/card/parse/token/lexicon/Article.java
@@ -1,9 +1,18 @@
 package tegenton.card.parse.token.lexicon;
 
+/**
+ * Articles are words that define specificity, such as "a" card or "the" card.
+ */
 public enum Article implements Word {
     a,
     an;
 
+    /**
+     * Returns the article represented by s.
+     *
+     * @param s string that may contain an article
+     * @return article contained in s, if s represents one, otherwise null
+     */
     public static Article fromString(String s) {
         for (Article type : Article.values()) {
             if (type.getWord().equalsIgnoreCase(s)) {
