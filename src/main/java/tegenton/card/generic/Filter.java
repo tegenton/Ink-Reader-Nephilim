dee37306033b863789bc2e192674c9b8ed46f1bc
Tim Egenton <22758764+tegenton@users.noreply.github.com>
2021-10-09 22:37:18 -0400

    Generic package now fully linted

--- src/main/java/tegenton/card/generic/Filter.java
+++ src/main/java/tegenton/card/generic/Filter.java
@@ -3,16 +3,34 @@ package tegenton.card.generic;
 import java.util.List;
 import java.util.function.Function;
 
-public class Filter<T> {
+/**
+ * Filter uses a list of functions that convert from String to a given type.
+ *
+ * @param <T> the type you would like to filter strings to
+ */
+public final class Filter<T> {
     private final List<Function<String, T>> filter;
 
-    public Filter(List<Function<String, T>> methods) {
+    /**
+     * Create a new filter with the given list of methods. These methods should
+     * return null if given an invalid string.
+     *
+     * @param methods functions that convert from String to T
+     */
+    public Filter(final List<Function<String, T>> methods) {
         filter = methods;
     }
 
-    public T filter(String s) {
+    /**
+     * Calls each of the given methods with s as a parameter.
+     *
+     * @param s The string to filter
+     * @return The first non-null object returned by one of the given methods,
+     * or null if all methods return null.
+     */
+    public T filter(final String s) {
         T result;
-        for (Function<String, T> test : filter) {
+        for (final Function<String, T> test : filter) {
             result = test.apply(s);
             if (result != null) {
                 return result;
--- src/main/java/tegenton/card/generic/StringSplitter.java
+++ src/main/java/tegenton/card/generic/StringSplitter.java
@@ -1,19 +1,39 @@
 package tegenton.card.generic;
 
-public class StringSplitter {
+/**
+ * Split a string into a pseudo stream.
+ */
+public final class StringSplitter {
     private final String delim = " ,.";
     private String string;
 
-    public StringSplitter(String s) {
+    /**
+     * Create a new string splitter with the given string.
+     *
+     * @param s a string to split
+     */
+    public StringSplitter(final String s) {
         string = s;
     }
 
+    /**
+     * If this method returns true, a call to getNext should return a non-empty
+     * string.
+     *
+     * @return are there any characters left in the string?
+     */
     public boolean hasNext() {
         return !string.isEmpty();
     }
 
+    /**
+     * The string of characters between the last value returned and the next
+     * instance of a delimiter character.
+     *
+     * @return substring of the original string
+     */
     public String getNext() {
-        StringBuilder token = new StringBuilder();
+        final StringBuilder token = new StringBuilder();
         for (int i = 0; i < string.length(); i++) {
             if (string.charAt(i) != ' ') {
                 token.append(string.charAt(i));
