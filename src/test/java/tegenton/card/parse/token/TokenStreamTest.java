4333e20a24bbe2b8dccd7c206a4f4f5c122d5ffa
Tim Egenton <22758764+tegenton@users.noreply.github.com>
2021-10-12 18:22:28 -0400

    Token Stream (issue #17)

---
+++ src/test/java/tegenton/card/parse/token/TokenStreamTest.java
@@ -0,0 +1,45 @@
+package tegenton.card.parse.token;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TokenStreamTest {
+
+    public static TokenStream tokenStream;
+    public static List<Token> expected;
+
+    @BeforeEach
+    void setup() {
+        expected = new ArrayList<>();
+    }
+
+    @AfterEach
+    void compare() {
+        int i = 0;
+        while (tokenStream.hasNext()) {
+            Assertions.assertEquals(expected.get(i++).getWord().getWord(), tokenStream.getNext().getWord().getWord());
+        }
+    }
+
+    @Test
+    void divination() {
+        tokenStream = new TokenStream("Draw two cards.");
+        expected.add(VerbToken.fromString("Draw"));
+        expected.add(EnglishNumberToken.fromString("Two"));
+        expected.add(SubjectToken.fromString("cards"));
+        expected.add(PunctuationToken.fromString("."));
+    }
+
+    @Test
+    void healersHawk() {
+        tokenStream = new TokenStream("Flying, Lifelink");
+        expected.add(KeywordAbilityToken.fromString("Flying"));
+        expected.add(PunctuationToken.fromString(","));
+        expected.add(KeywordAbilityToken.fromString("Lifelink"));
+    }
+}
