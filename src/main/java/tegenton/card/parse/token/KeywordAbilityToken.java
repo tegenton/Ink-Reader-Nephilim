c53399034998e668fa4b63023f423d3894f4fb44
Tim Egenton <22758764+tegenton@users.noreply.github.com>
2021-10-12 22:28:56 -0400

    make KeywordAbilityToken public

--- src/main/java/tegenton/card/parse/token/KeywordAbilityToken.java
+++ src/main/java/tegenton/card/parse/token/KeywordAbilityToken.java
@@ -2,7 +2,7 @@ package tegenton.card.parse.token;
 
 import tegenton.card.parse.token.lexicon.KeywordAbility;
 
-final class KeywordAbilityToken extends Token {
+public final class KeywordAbilityToken extends Token {
     private KeywordAbilityToken(final KeywordAbility word) {
         this.setWord(word);
     }
