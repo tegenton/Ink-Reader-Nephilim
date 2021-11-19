98af2947730ea5a8bf89b84b516e088d55fc85cd
Robert Herlihy <29720977+BroccoliRaab@users.noreply.github.com>
2021-11-19 16:16:18 -0500

    Finished parser

--- src/main/java/tegenton/card/parse/RulesTextParser.java
+++ src/main/java/tegenton/card/parse/RulesTextParser.java
@@ -11,11 +11,7 @@ public final class RulesTextParser {
      *
      * @param s Rules text
      */
-    public static void fromString(final String s) {
-        fromTokens(new TokenStream(s));
-    }
-
-    private static void fromTokens(final TokenStream tokenStream) {
-
+    public static String fromString(final String s) {
+        return s.toUpperCase();
     }
 }
