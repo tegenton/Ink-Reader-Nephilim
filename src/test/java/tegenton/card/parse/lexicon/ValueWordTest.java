4adc1e3a7d5d7530949f201ee631644dc8bebbad
tegenton <22758764+tegenton@users.noreply.github.com>
2021-12-10 14:42:12 -0500

    Test generic value class

--- src/test/java/tegenton/card/parse/lexicon/ValueWordTest.java
+++ src/test/java/tegenton/card/parse/lexicon/ValueWordTest.java
@@ -5,26 +5,39 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ValueWordTest {
+    public static List<String> words = List.of("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+            "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty");
+
+    public static List<String> digits = List.of("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+            "11", "12", "13", "14", "15", "16", "17", "18", "19", "20");
 
     @Test
     void englishNumber() {
-        List<String> strings = List.of("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
-                "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty");
         EnglishNumber number;
-        for (int i = 0; i < strings.size(); i++) {
-            number = EnglishNumber.valueOf(strings.get(i).toUpperCase());
+        for (int i = 0; i < words.size(); i++) {
+            number = EnglishNumber.valueOf(words.get(i).toUpperCase());
             assertEquals(number.getVal(), i);
         }
     }
 
     @Test
     void number() {
-        List<String> strings = List.of("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
-                "11", "12", "13", "14", "15", "16", "17", "18", "19", "20");
         Number number;
-        for (int i = 0; i < strings.size(); i++) {
-            number = Number.valueOf(strings.get(i));
+        for (int i = 0; i < digits.size(); i++) {
+            number = Number.valueOf(digits.get(i));
             assertEquals(number.getVal(), i);
+        }
+    }
+
+    @Test
+    void values() {
+        assertEquals(digits.size(), words.size());
+        ValueWord digit;
+        ValueWord word;
+        for (int i = 0; i < digits.size(); i++) {
+            digit = Number.valueOf(digits.get(i));
+            word = EnglishNumber.valueOf(words.get(i).toUpperCase());
+            assertEquals(digit.getVal(), word.getVal());
         }
     }
 }
