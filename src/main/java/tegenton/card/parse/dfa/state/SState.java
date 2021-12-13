e2ae2fa5381d55bf0a4f39694c16614b8082520e
tegenton <22758764+tegenton@users.noreply.github.com>
2021-12-13 09:41:00 -0500

    S
    closes #35

---
+++ src/main/java/tegenton/card/parse/dfa/state/SState.java
@@ -0,0 +1,30 @@
+package tegenton.card.parse.dfa.state;
+
+import tegenton.card.parse.lexicon.Morpheme;
+import tegenton.card.parse.lexicon.Word;
+import java.util.Optional;
+
+public class SState extends State {
+    private static final SState instance = new SState();
+
+    public static SState state() {
+        return instance;
+    }
+
+    @Override
+    public State transition(char c) {
+        return switch (c) {
+            case '\0', ' ' -> SpaceState.state();
+            case '.' -> PeriodState.state();
+            default -> throw new IllegalStateException("Cannot transition from SState on " + c);
+        };
+    }
+
+    @Override
+    public Optional<? extends Word> produce(char c) {
+        return switch (c) {
+            case '\0', ' ', '.' -> Optional.of(Morpheme.S);
+            default -> Optional.empty();
+        };
+    }
+}
